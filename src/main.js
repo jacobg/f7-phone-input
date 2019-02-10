ad1c114aea64449df076029042824ddaaaa71a38
Jacob Gur <jacob@fareclock.com>
2019-02-10 12:42:15 -0500

    fixes

--- src/main.js
+++ src/main.js
@@ -7,6 +7,8 @@ import Framework7 from 'framework7/framework7.esm.bundle.js'
 // Import F7-Vue Plugin Bundle (with all F7 components registered)
 import Framework7Vue from 'framework7-vue/framework7-vue.esm.bundle.js'
 
+Vue.config.devtools = true
+
 Framework7.use(Framework7Vue)
 
 new Vue({
